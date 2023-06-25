05ca362cc8cd399b8b67ad48630f6d73f40e8275
Antía <antiabofan@gmail.com>
2023-06-25 17:51:39 +0200

    Named "blog" collection as "posts", tags in schema

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,6 +1,6 @@
 import { defineCollection, z } from 'astro:content';
 
-const blog = defineCollection({
+const posts = defineCollection({
 	// Type-check frontmatter using a schema
 	schema: z.object({
 		title: z.string(),
@@ -15,7 +15,8 @@ const blog = defineCollection({
 			.optional()
 			.transform((str) => (str ? new Date(str) : undefined)),
 		heroImage: z.string().optional(),
+		tags: z.array(z.string())
 	}),
 });
 
-export const collections = { blog };
+export const collections = { posts };
