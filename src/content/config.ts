7aa648768165e28bd3db2023a9386db129596dea
Antía <antiabofan@gmail.com>
2023-07-20 19:02:44 +0200

    Updated posts collection schema

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,10 +1,11 @@
-import { defineCollection, reference, z } from 'astro:content';
+import { defineCollection, z } from 'astro:content';
 
 const posts = defineCollection({
 	// Type-check frontmatter using a schema
 	type: "content",
 	schema: z.object({
 		title: z.string(),
+		author: z.string().optional(),
 		summary: z.string(),
 		// Transform string to Date object
 		pubDate: z
@@ -12,8 +13,8 @@ const posts = defineCollection({
 			.or(z.date())
 			.transform((val) => new Date(val)),
 		heroImage: z.string().optional(),
+		heroImageAltText: z.string().optional(),
 		tags: z.array(z.string()),
-		relatedPosts: z.array(reference('blog')).optional(),
 		url: z.string().optional()
 	}),
 });
